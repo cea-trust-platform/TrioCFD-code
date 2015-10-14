12067e49ef710f9e0123e7b4264d9c4f440a824c
triou <triou@cea.fr>
2015-10-14 11:37:29 +0200

    UtilitairesAssemblages dependency deleted because not used by this baltik (moved in ModeleCoeur baltik)

--- share/Test.sh
+++ share/Test.sh
@@ -5,8 +5,8 @@ baltik_build_configure
 ./configure
 time make debug dist_clean
 
-for dir in ALE Front_tracking_discontinu LES Phase_field  Rayonnement Rayonnement_semi_transp UtilitairesAssemblages Zoom validation K_Eps_non_std
-#for dir in  LES  UtilitairesAssemblages Zoom validation
+for dir in ALE Front_tracking_discontinu LES Phase_field  Rayonnement Rayonnement_semi_transp Zoom validation K_Eps_non_std
+#for dir in  LES  Zoom validation
 do
 cd $dir
 baltik_build_configure
