7628583c54408a0f329ca3533abd0f668e50fee9
Pierre LEDAC <pierre.ledac@cea.fr>
2020-06-25 16:34:53 +0200

    Fix crash on Intel

--- Front_tracking_discontinu/src/Transport_Interfaces_FT_Disc.cpp
+++ Front_tracking_discontinu/src/Transport_Interfaces_FT_Disc.cpp
@@ -3159,30 +3159,22 @@ void Transport_Interfaces_FT_Disc::calculer_distance_interface_faces(
     }
   // Division par le nombre d'elements voisins
   const double valeur_invalide = distance_faces_invalides * 1.1;
+#ifdef __INTEL_COMPILER
+#pragma novector // Desactive vectorisation sur Intel car crash sinon
+#endif
   for (i = 0; i < nb_faces; i++)
     {
       const int n = ncontrib[i];
       if (n > 0)
-        {
-          // const double x = 1. / (double) n;  // Fixed bug: Arithmetic exception
-          // dist_face(i) *= x;
-          const double dn = (double) n;
-          if (dabs(dn)>=DMINFLOAT)
-            {
-              const double x = 1. / dn;
-              dist_face(i) *= x;
-            }
-        }
+        dist_face(i) /= n;
       else
-        {
-          dist_face(i) = valeur_invalide;
-        }
+        dist_face(i) = valeur_invalide;
     }
   dist_face.echange_espace_virtuel();
 }
 
 
-const Champ_base&   Transport_Interfaces_FT_Disc::get_update_distance_interface_faces() const
+const Champ_base& Transport_Interfaces_FT_Disc::get_update_distance_interface_faces() const
 {
   // Si le tag du maillage et le tag du champ sont identiques, inutile de recalculer:
   const int tag = maillage_interface().get_mesh_tag();
@@ -7455,24 +7447,16 @@ void Transport_Interfaces_FT_Disc::calculer_distance_interface_sommets(
     }
   // Division par le nombre d'elements voisins
   const double valeur_invalide = distance_sommets_invalides * 1.1;
+#ifdef __INTEL_COMPILER
+#pragma novector // Desactive vectorisation sur Intel car crash sinon
+#endif
   for (i = 0; i < nb_sommets; i++)
     {
       const int n = ncontrib[i];
       if (n > 0)
-        {
-          // const double x = 1. / (double) n; // Fixed bug: Arithmetic exception
-          // dist_som(i) *= x;
-          const double dn = (double) n;
-          if (dabs(dn)>=DMINFLOAT)
-            {
-              const double x = 1. / dn;
-              dist_som(i) *= x;
-            }
-        }
+        dist_som(i) /= n;
       else
-        {
-          dist_som(i) = valeur_invalide;
-        }
+        dist_som(i) = valeur_invalide;
     }
   dist_som.echange_espace_virtuel();
   Debog::verifier("Transport_Interfaces_FT_Disc::calculer_distance_interface_sommets",dist_som);
