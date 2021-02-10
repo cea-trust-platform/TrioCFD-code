86536099350af5a20883c013e982a088c5e48c4b
adela <maria-adela.puscas@cea.fr>
2021-02-10 12:34:08 +0100

    Update .prm

--- ALE/share/Validation/Rapports_automatiques/3dOscillatingBeam/src/ForceNum.py
+++ ALE/share/Validation/Rapports_automatiques/3dOscillatingBeam/src/ForceNum.py
@@ -14,23 +14,17 @@ Lmax  = 0.7 ; #length of the beam
 Ri=0.02
 u= 0.5*1.e-4; #module of displacement 
 w = 1000.9   #pulsation of displacement
-f=w/2.*np.pi #frequency of displacement
+f=w/(2.*np.pi) #frequency of displacement
 T=1./f #period of displacement
-nb_p=1; # number of periods
+nb_p=5.; # number of periods
 
 
 # Calculation of the fluid force
 t= np.loadtxt(test_case+'Force_pression.out', unpack=True, usecols=[0])
-t_adim = w*t 
 
 Fpy = np.loadtxt(test_case+'Force_pression.out', unpack=True, usecols=[8])
 Fvy = np.loadtxt(test_case+'Contrainte_visqueuse.out', unpack=True, usecols=[8])
 Fy = Fvy + Fpy 
-
-Fy_adim = Fy/(rho*Ri**2.*u*w**2.) 
-
-DataOut = np.column_stack((t_adim,Fy_adim))
-np.savetxt('Numerical_force_adim.txt', DataOut)
 
 DataOut = np.column_stack((t,Fy))
 np.savetxt('Numerical_force.txt', DataOut)
@@ -42,12 +36,12 @@ np.savetxt('Numerical_force.txt', DataOut)
 
 
 
-CMNumy=2./nb_p/T*np.trapz(np.sin(w*t)*Fy,t)/(u*w**2.)
-CVNumy =- 2./nb_p/T*np.trapz(np.cos(w*t)*Fy,t)/(u*w)
+CMNumy=2./nb_p/T*np.trapz(np.sin(w*t)*Fy,t)
+CVNumy =- 2./nb_p/T*np.trapz(np.cos(w*t)*Fy,t)
 
 
-CmNumy = CMNumy/(Lmax*rho*Ri**2.*np.pi)
-CvNumy = CVNumy/(Lmax*rho*Ri**2.*np.pi*w)
+CmNumy = CMNumy/(Lmax*rho*Ri**2.*np.pi*u*w**2.)
+CvNumy = CVNumy/(Lmax*rho*Ri**2.*np.pi*u*w**2.)
 
 # print('Added mass coefficient Cmy =', CmNumy)
 # print('Added dumping coefficient Cvy =', CvNumy)
@@ -59,7 +53,7 @@ np.savetxt('Numerical_coefficients.txt', DataOut1)
 # Save DataOut in .txt file
 
 Cm=10.16
-Cv=0.26
+Cv=0.6
 F = -rho*np.pi*Lmax*Ri**2.*u*w**2.*(-Cm*np.sin(w*t) + Cv*np.cos(w*t)) #Force vector
 DataOut = np.column_stack((t,F))
 np.savetxt('Theoretical_force.txt', DataOut) #this command overwrite the previus .txt file
