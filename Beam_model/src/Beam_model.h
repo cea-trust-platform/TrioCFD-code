1f0829705191a0daec06efd678da167c480489a2
adela <maria-adela.puscas@cea.fr>
2021-03-16 15:50:36 +0100

    Define teh Beam_model class

---
+++ Beam_model/src/Beam_model.h
@@ -0,0 +1,127 @@
+/****************************************************************************
+* Copyright (c) 2021, CEA
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+* OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*****************************************************************************/
+/////////////////////////////////////////////////////////////////////////////
+//
+// File      : Beam_model.h
+// Directory : $BEAM_MODEL_ROOT/src
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef Beam_model_included
+#define Beam_model_included
+
+#include <Interprete_geometrique_base.h>
+#include <Nom.h>
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// .DESCRIPTION : class Beam_model
+//
+// <Description of class Beam_model>
+//
+/////////////////////////////////////////////////////////////////////////////
+
+class Beam_model : public Interprete_geometrique_base
+{
+
+  Declare_instanciable( Beam_model ) ;
+
+public :
+  Entree& interpreter_(Entree&);
+  inline const int& getNbModes() const;
+  inline void setNbModes(const int&) ;
+  inline const int& getDirection() const;
+  inline void setDirection(const int&) ;
+  inline const double& getYoung() const;
+  inline void setYoung(const double&);
+  inline const double& getRhoBeam() const;
+  inline void setRhoBeam(const double&) ;
+  inline const Nom& getBeamName() const;
+  inline void setBeamName(const Nom&) ;
+  inline const Nom& getFileName() const;
+  inline void setFileName(const Nom&) ;
+  inline const bool& getActivate() const;
+  inline void setActivate(const bool&) ;
+protected :
+  int nb_modes_;
+  int direction_; //x=1, y=2, z=3
+  double young_; // Young module
+  double rho_; // solid density
+  Nom name_of_beam_;
+  Nom masse_and_phi_file_name_;
+  bool activate_=false;
+
+};
+inline const int& Beam_model::getNbModes() const
+{
+  return nb_modes_;
+}
+inline void Beam_model::setNbModes(const int& modes)
+{
+  nb_modes_=modes;
+}
+inline const int& Beam_model::getDirection() const
+{
+  return direction_;
+}
+inline void Beam_model::setDirection(const int& direction)
+{
+  direction_=direction;
+}
+inline const double& Beam_model::getYoung() const
+{
+  return young_;
+}
+inline void Beam_model::setYoung(const double& young)
+{
+  young_ = young;
+}
+inline const double& Beam_model::getRhoBeam() const
+{
+  return rho_;
+}
+inline void Beam_model::setRhoBeam(const double& rho)
+{
+  rho_=rho;
+}
+
+inline const Nom& Beam_model::getBeamName() const
+{
+  return name_of_beam_;
+}
+
+inline void Beam_model::setBeamName(const Nom& name)
+{
+  name_of_beam_= name;
+}
+inline const Nom& Beam_model::getFileName() const
+{
+  return masse_and_phi_file_name_;
+}
+inline void Beam_model::setFileName(const Nom& name)
+{
+  masse_and_phi_file_name_=name;
+}
+
+inline const bool& Beam_model::getActivate() const
+{
+  return activate_;
+}
+inline void Beam_model::setActivate(const bool& activate)
+{
+  activate_=activate;
+}
+
+#endif /* Beam_model_included */
