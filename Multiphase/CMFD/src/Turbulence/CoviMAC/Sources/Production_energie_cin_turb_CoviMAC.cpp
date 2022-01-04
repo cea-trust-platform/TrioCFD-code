d3a338f0ba0cd6445882d4c4730aaef15181fd94
Corentin Reiss <cr268029@is224592.intra.cea.fr>
2022-01-04 15:59:49 +0100

    [CoviMAC] Source term for turbulent kinetic energy production

---
+++ Multiphase/CMFD/src/Turbulence/CoviMAC/Sources/Production_energie_cin_turb_CoviMAC.cpp
@@ -0,0 +1,78 @@
+/****************************************************************************
+* Copyright (c) 2021, CEA
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+* OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*****************************************************************************/
+//////////////////////////////////////////////////////////////////////////////
+//
+// File:        Production_energie_cin_turb_CoviMAC.cpp
+// Directory:   $TRUST_ROOT/src/ThHyd/Multiphase/CoviMAC
+// Version:     /main/13
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include "Production_energie_cin_turb_CoviMAC.h"
+
+#include <Zone_CoviMAC.h>
+#include <Champ_P0_CoviMAC.h>
+#include <Matrix_tools.h>
+#include <Pb_Multiphase.h>
+#include <grad_Champ_Face_CoviMAC.h>
+#include <Champ_Uniforme.h>
+#include <Flux_interfacial_base.h>
+#include <Milieu_composite.h>
+#include <Operateur_Diff.h>
+#include <Op_Diff_Turbulent_CoviMAC_Face.h>
+#include <QDM_Multiphase.h>
+#include <Viscosite_turbulente_base.h>
+
+Implemente_instanciable(Production_energie_cin_turb_CoviMAC,"Production_energie_cin_turb_P0_CoviMAC", Source_base);
+
+Sortie& Production_energie_cin_turb_CoviMAC::printOn(Sortie& os) const
+{
+  return os;
+}
+
+Entree& Production_energie_cin_turb_CoviMAC::readOn(Entree& is)
+{
+  return is;
+}
+
+void Production_energie_cin_turb_CoviMAC::dimensionner_blocs(matrices_t matrices, const tabs_t& semi_impl) const
+{
+// empty : no derivative for the turbulent kinetic energy production to add in the blocks
+}
+
+void Production_energie_cin_turb_CoviMAC::ajouter_blocs(matrices_t matrices, DoubleTab& secmem, const tabs_t& semi_impl) const
+{
+  const Zone_CoviMAC&                      zone = ref_cast(Zone_CoviMAC, equation().zone_dis().valeur());
+  const Pb_Multiphase&                       pb = ref_cast(Pb_Multiphase, equation().probleme());
+  const QDM_Multiphase&                  eq_qdm = ref_cast(QDM_Multiphase, pb.equation(0));
+  const grad_Champ_Face_CoviMAC&           grad = ref_cast(grad_Champ_Face_CoviMAC, eq_qdm.get_champ("gradient_vitesse"));
+  const DoubleTab&                     tab_grad = grad.valeurs();
+  const Op_Diff_Turbulent_CoviMAC_Face& Op_diff = ref_cast(Op_Diff_Turbulent_CoviMAC_Face, eq_qdm.operateur(0).l_op_base());
+  const Viscosite_turbulente_base&    visc_turb = ref_cast(Viscosite_turbulente_base, Op_diff.corr.valeur());
+
+  int N = pb.nb_phases(), nf_tot = zone.nb_faces_tot(), ne = zone.nb_elem(), D = dimension ;
+
+  DoubleTrav Rij(0, N, D, D);
+  MD_Vector_tools::creer_tableau_distribue(eq_qdm.pression()->valeurs().get_md_vector(), Rij); //
+  visc_turb.reynolds_stress(Rij);
+  assert((ne == Rij.dimension(0)));
+
+  int n = 0 ; // the only kinetic energy production is in phase 0
+
+  for(int e = 0 ; e < ne ; e++) for (int d_U = 0; d_U < D; d_U++) for (int d_X = 0; d_X < D; d_X++)
+        {
+          secmem(e, n) += Rij(e, n, d_X, d_U) * tab_grad(nf_tot + d_X + e * D , D * n + d_U) ;
+        }
+}
