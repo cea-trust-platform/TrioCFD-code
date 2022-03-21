594fb33e5930e8907ca79f41911da0f8b0549928
REISS Corentin <cr268029@is245493.intra.cea.fr>
2022-03-21 16:46:50 +0100

    [turbulence covimac] bugfix detecte avec fiche jupyter

--- Multiphase/CMFD/src/Turbulence/CoviMAC/Sources/Production_echelle_temp_taux_diss_turb_CoviMAC.cpp
+++ Multiphase/CMFD/src/Turbulence/CoviMAC/Sources/Production_echelle_temp_taux_diss_turb_CoviMAC.cpp
@@ -174,7 +174,7 @@ void Production_echelle_temp_taux_diss_turb_CoviMAC::ajouter_blocs(matrices_t ma
         if (Type_diss == "tau")
           secmem_en *= pe(e) * ve(e) *      alpha_omega_* tab_alp(e, n) * tab_rho(e, n)*tab_diss(e, n)*tab_diss(e, n)/std::max(tab_k(e, n) * tab_diss(e, n), visc_turb.limiteur() * nu(e, n)) ;
         else if (Type_diss == "omega")
-          secmem_en *= pe(e) * ve(e) * (-1)*alpha_omega_* tab_alp(e, n) * tab_rho(e, n)* ((tab_diss(e, n) <= 0) ? 0 : (std::max(tab_k(e, n)/tab_diss(e, n), visc_turb.limiteur() * nu(e, n)))) ;
+          secmem_en *= pe(e) * ve(e) * (-1)*alpha_omega_* tab_alp(e, n) * tab_rho(e, n)* ((tab_diss(e, n) <= 0) ? 0 : 1/(std::max(tab_k(e, n)/tab_diss(e, n), visc_turb.limiteur() * nu(e, n)))) ;
         secmem(e, n) += secmem_en;
       }
 }
