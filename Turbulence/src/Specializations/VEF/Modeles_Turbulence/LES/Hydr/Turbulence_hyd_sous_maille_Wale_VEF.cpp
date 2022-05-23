052996d3de795ee109c64b84facaa70b49badeca
Pierre LEDAC <pierre.ledac@cea.fr>
2022-05-23 15:26:30 +0200

    [rocALUTION] Add test for TrioCFD + Wale OpenMP OK

--- Turbulence/src/Specializations/VEF/Modeles_Turbulence/LES/Hydr/Turbulence_hyd_sous_maille_Wale_VEF.cpp
+++ Turbulence/src/Specializations/VEF/Modeles_Turbulence/LES/Hydr/Turbulence_hyd_sous_maille_Wale_VEF.cpp
@@ -71,103 +71,91 @@ Champ_Fonc& Turbulence_hyd_sous_maille_Wale_VEF::calculer_viscosite_turbulente()
   double temps = mon_equation->inconnue().temps();
   DoubleTab& visco_turb = la_viscosite_turbulente.valeurs();
   const int nb_elem = zone_VEF.nb_elem();
-  const int nb_elem_tot = zone_VEF.nb_elem_tot();
-
-  OP1.resize(nb_elem_tot);  // OP1 est le premier operateur spatial du modele WALE.
-  OP2.resize(nb_elem_tot);  // OP2 est le deuxieme operateur spatial du modele WALE.
-
-  calculer_OP1_OP2();
+  const DoubleTab& la_vitesse = mon_equation->inconnue().valeurs();
+  const Zone_Cl_VEF& zone_Cl_VEF = la_zone_Cl_VEF.valeur();
 
   if (visco_turb.size() != nb_elem)
     {
       Cerr << "Size error for the array containing the values of the turbulent viscosity." << finl;
       exit();
     }
-
-
   Debog::verifier("Turbulence_hyd_sous_maille_Wale_VEF::calculer_viscosite_turbulente visco_turb 0",visco_turb);
 
-  for (int elem=0; elem<nb_elem; elem++)
-    {
-      if (OP1[elem]!=0.) // donc sd2 et OP2 par voie de consequence sont differents de zero
-        visco_turb[elem]=cw*cw*l_(elem)*l_(elem)*OP1[elem]/OP2[elem];
-      else
-        visco_turb[elem]=0;
-    }
+  // Patrick : on travaille sur le champ filtre.
+  //const Champ_P1NC& ch=(const Champ_P1NC&) mon_equation->inconnue().valeur();
+  DoubleTab ubar(la_vitesse);
+  //  ch.filtrer_L2(ubar);
+  const int nb_elem_tot = zone_VEF.nb_elem_tot();
+  DoubleTab duidxj(nb_elem_tot,dimension,dimension);
+  Champ_P1NC::calcul_gradient(ubar,duidxj,zone_Cl_VEF);
+
+  // OpenMP ToDo:
+  // l_ passe une seule fois dans une region
+  // Utiliser copyToDevice pour duidxj_addr et l_addr
+  double* visco_turb_addr=visco_turb.addr();
+  const double* l_addr=l_.addr();
+  const double* duidxj_addr=duidxj.addr();
+  #pragma omp target teams map(to:duidxj_addr[0:duidxj.size_array()],l_addr[0:l_.size_array()]) map(from:visco_turb_addr[0:visco_turb.size_array()])
+  {
+    double gij2[3][3] {};
+    double sd[3][3] {};
+    #pragma omp distribute parallel for private(gij2, sd)
+    for (int elem=0; elem<nb_elem; elem ++)
+      {
+
+        // Calcul du terme gij2.
+        for(int i=0; i<dimension; i++)
+          for(int j=0; j<dimension; j++)
+            {
+              gij2[i][j]=0;
+              for(int k=0; k<dimension; k++)
+                gij2[i][j]+=duidxj_addr[elem*dimension*dimension+i*dimension+k] * duidxj_addr[elem*dimension*dimension+k*dimension+j];
+            }
+
+        // Calcul du terme gkk2.
+        double gkk2=0;
+        for(int k=0; k<dimension; k++)
+          gkk2 += gij2[k][k];
+
+        // Calcul de sd.
+        for(int i=0; i<dimension; i++)
+          for(int j=0; j<dimension; j++)
+            {
+              sd[i][j]=0.5*(gij2[i][j]+gij2[j][i]);
+              if(i==j)
+                {
+                  sd[i][j]-=gkk2/3.; // Terme derriere le tenseur de Kronecker.
+                }
+            }
+
+        // Calcul de sd2 et Sij2.
+        double sd2=0.;
+        double Sij2=0.;
+        for (int i=0 ; i<dimension ; i++)
+          for (int j=0 ; j<dimension ; j++)
+            {
+              sd2+=sd[i][j]*sd[i][j];
+              //Deplacement du calcul de Sij
+              double Sij=0.5*(duidxj_addr[elem*dimension*dimension+i*dimension+j] + duidxj_addr[elem*dimension*dimension+j*dimension+i]);
+              Sij2+=Sij*Sij;
+            }
+
+        // Calcul de OP1 et OP2.
+        // Replace pow by sqrt and multiply, faster
+        //OP1=pow(sd2,1.5);
+        double OP1=sd2*sqrt(sd2);
+        //OP2=pow(Sij2,2.5)+pow(sd2,1.25);
+        double OP2=Sij2*Sij2*sqrt(Sij2)+sd2*sqrt(sqrt(sd2));
+
+        if (OP1!=0.) // donc sd2 et OP2 par voie de consequence sont differents de zero
+          visco_turb_addr[elem]=cw*cw*l_addr[elem]*l_addr[elem]*OP1/OP2;
+        else
+          visco_turb_addr[elem]=0;
+      }// fin de la boucle sur les elements
+  }
 
   Debog::verifier("Turbulence_hyd_sous_maille_Wale_VEF::calculer_viscosite_turbulente visco_turb 1",visco_turb);
 
   la_viscosite_turbulente.changer_temps(temps);
   return la_viscosite_turbulente;
 }
-
-void Turbulence_hyd_sous_maille_Wale_VEF::calculer_OP1_OP2()
-{
-  const DoubleTab& la_vitesse = mon_equation->inconnue().valeurs();
-  const Zone_Cl_VEF& zone_Cl_VEF = la_zone_Cl_VEF.valeur();
-  const Zone_VEF& zone_VEF = la_zone_VEF.valeur();
-  const int nb_elem = zone_VEF.nb_elem_tot();
-
-  DoubleTrav gij2(dimension,dimension);
-  DoubleTrav sd(dimension,dimension);
-
-  // Patrick : on travaille sur le champ filtre.
-  //const Champ_P1NC& ch=(const Champ_P1NC&) mon_equation->inconnue().valeur();
-  DoubleTab ubar(la_vitesse);
-  //  ch.filtrer_L2(ubar);
-
-
-  DoubleTab duidxj(nb_elem,dimension,dimension);
-
-  Champ_P1NC::calcul_gradient(ubar,duidxj,zone_Cl_VEF);
-
-  for (int elem=0; elem<nb_elem; elem ++)
-    {
-
-      // Calcul du terme gij2.
-      for(int i=0; i<dimension; i++)
-        for(int j=0; j<dimension; j++)
-          {
-            gij2(i,j)=0;
-            for(int k=0; k<dimension; k++)
-              gij2(i,j)+=duidxj(elem,i,k)*duidxj(elem,k,j);
-          }
-
-      // Calcul du terme gkk2.
-      double gkk2=0;
-      for(int k=0; k<dimension; k++)
-        gkk2 += gij2(k,k);
-
-      // Calcul de sd.
-      for(int i=0; i<dimension; i++)
-        for(int j=0; j<dimension; j++)
-          {
-            sd(i,j)=0.5*(gij2(i,j)+gij2(j,i));
-            if(i==j)
-              {
-                sd(i,j)-=gkk2/3.; // Terme derriere le tenseur de Kronecker.
-              }
-          }
-
-      // Calcul de sd2 et Sij2.
-      double sd2=0.;
-      double Sij2=0.;
-      for (int i=0 ; i<dimension ; i++)
-        for (int j=0 ; j<dimension ; j++)
-          {
-            sd2+=sd(i,j)*sd(i,j);
-            //Deplacement du calcul de Sij
-            double Sij=0.5*(duidxj(elem,i,j) + duidxj(elem,j,i));
-            Sij2+=Sij*Sij;
-          }
-
-      // Calcul de OP1 et OP2.
-      // Replace pow by sqrt and multiply, faster
-      //OP1(elem)=pow(sd2,1.5);
-      OP1(elem)=sd2*sqrt(sd2);
-      //OP2(elem)=pow(Sij2,2.5)+pow(sd2,1.25);
-      OP2(elem)=Sij2*Sij2*sqrt(Sij2)+sd2*sqrt(sqrt(sd2));
-
-    }// fin de la boucle sur les elements
-}
-
