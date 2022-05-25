721377ed97d25450b7cadea65c59c0236392fa3f
triou <triou@cea.fr>
2022-05-25 10:43:03 +0200

    Update patches since evolved in TRUST (unnecessary includes deleted)

--- Turbulence/src/TRUST_FIXES/CANNOT_BE_MOVED/IA/Operateur_Diff.cpp
+++ Turbulence/src/TRUST_FIXES/CANNOT_BE_MOVED/IA/Operateur_Diff.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2015 - 2016, CEA
+* Copyright (c) 2022, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -22,7 +22,6 @@
 #include <Operateur_Diff.h>
 #include <Operateur_Diff_base.h>
 #include <Champ_Don.h>
-#include <Champ_Uniforme.h>
 #include <Discretisation_base.h>
 #include <stat_counters.h>
 #include <Champ_base.h>
