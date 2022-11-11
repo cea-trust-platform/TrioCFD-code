618d40eee81923e6089435f21eb9a5ee46a9389b
Anida Khizar <anida.khizar@cea.fr>
2022-11-11 22:51:35 +0100

    [IJK] Old headers removed

--- Multiphase/Front_tracking_IJK/IJK_Kernel/src/FFT/Fourier_trans.h
+++ Multiphase/Front_tracking_IJK/IJK_Kernel/src/FFT/Fourier_trans.h
@@ -12,12 +12,7 @@
 * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 *****************************************************************************/
-/////////////////////////////////////////////////////////////////////////////
-//
-// File      : Fourier_trans.h
-// Directory : $NEW_ALGO_QC_ROOT/src
-//
-/////////////////////////////////////////////////////////////////////////////
+
 #ifndef Fourier_trans_H
 #define Fourier_trans_H
 #include <FixedVector.h>
--- Multiphase/Front_tracking_IJK/IJK_Kernel/src/Statistiques_dns_ijk_monophasique.cpp
+++ Multiphase/Front_tracking_IJK/IJK_Kernel/src/Statistiques_dns_ijk_monophasique.cpp
@@ -12,12 +12,7 @@
 * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 *****************************************************************************/
-/////////////////////////////////////////////////////////////////////////////
-//
-// File      : Statistiques_dns_ijk_monophasique.cpp
-// Directory : $IJK_ROOT/src
-//
-/////////////////////////////////////////////////////////////////////////////
+
 #include <Statistiques_dns_ijk_monophasique.h>
 #include <IJK_Grid_Geometry.h>
 #include <TRUSTTab.h>
