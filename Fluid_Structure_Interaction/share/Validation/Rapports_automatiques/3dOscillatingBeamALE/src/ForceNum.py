04a3249a4ce84352c7d43a79ad5414bc0e6ae4b4
PUSCAS Maria Adela <mp250145@is244542.intra.cea.fr>
2022-11-17 09:39:54 +0100

    Update the 3dOscillatingBeamALE prm following the rename of the validation sheet

--- Fluid_Structure_Interaction/share/Validation/Rapports_automatiques/3dOscillatingBeamALE/src/ForceNum.py
+++ Fluid_Structure_Interaction/share/Validation/Rapports_automatiques/3dOscillatingBeamALE/src/ForceNum.py
@@ -1,61 +1,61 @@
-#*********************** force and coeff. num.*********************
+#*********************** Force and Coeff. Num.*********************
 #******************************************************************************
 
-# import library
+# Import library
 import numpy as np
 
-test_case = "3doscillatingbeamALE_pb_"
+test_case = "3dOscillatingBeamALE_pb_"
 
 rho = 1000. #fluid density
 nu = 1.007e-6 #fluid viscosity
 
-# definition of displacement
-lmax  = 0.7 ; #length of the beam
-ri=0.02
+# Definition of displacement
+Lmax  = 0.7 ; #length of the beam
+Ri=0.02
 u= 0.5*1.e-4; #module of displacement 
 w = 1000.9   #pulsation of displacement
 f=w/(2.*np.pi) #frequency of displacement
-t=1./f #period of displacement
+T=1./f #period of displacement
 nb_p=5.; # number of periods
 
 
-# calculation of the fluid force
-t= np.loadtxt(test_case+'force_pression.out', unpack=true, usecols=[0])
+# Calculation of the fluid force
+t= np.loadtxt(test_case+'Force_pression.out', unpack=True, usecols=[0])
 
-fpy = np.loadtxt(test_case+'force_pression.out', unpack=true, usecols=[8])
-fvy = np.loadtxt(test_case+'contrainte_visqueuse.out', unpack=true, usecols=[8])
-fy = fvy + fpy 
+Fpy = np.loadtxt(test_case+'Force_pression.out', unpack=True, usecols=[8])
+Fvy = np.loadtxt(test_case+'Contrainte_visqueuse.out', unpack=True, usecols=[8])
+Fy = Fvy + Fpy 
 
-dataout = np.column_stack((t,fy))
-np.savetxt('numerical_force.txt', dataout)
-
-
-
-
-# compute the fluid added mass and fluid added damping coeff.
+DataOut = np.column_stack((t,Fy))
+np.savetxt('Numerical_force.txt', DataOut)
 
 
 
-cmnumy=2./nb_p/t*np.trapz(np.sin(w*t)*fy,t)
-cvnumy =- 2./nb_p/t*np.trapz(np.cos(w*t)*fy,t)
+
+# Compute the fluid added mass and fluid added damping coeff
 
 
-cmnumy = cmnumy/(lmax*rho*ri**2.*np.pi*u*w**2.)
-cvnumy = cvnumy/(lmax*rho*ri**2.*np.pi*u*w**2.)
 
-# print('added mass coefficient cmy =', cmnumy)
-# print('added dumping coefficient cvy =', cvnumy)
-
-dataout1 = np.column_stack((cmnumy,cvnumy))
-np.savetxt('numerical_coefficients.txt', dataout1)
+CMNumy=2./nb_p/T*np.trapz(np.sin(w*t)*Fy,t)
+CVNumy =- 2./nb_p/T*np.trapz(np.cos(w*t)*Fy,t)
 
 
-# save dataout in .txt file
+CmNumy = CMNumy/(Lmax*rho*Ri**2.*np.pi*u*w**2.)
+CvNumy = CVNumy/(Lmax*rho*Ri**2.*np.pi*u*w**2.)
 
-cm=10.16
-cv=0.
-f = -rho*np.pi*lmax*ri**2.*u*w**2.*(-cm*np.sin(w*t) + cv*np.cos(w*t)) #force vector
-dataout = np.column_stack((t,f))
-np.savetxt('theoretical_force.txt', dataout) #this command overwrite the previus .txt file
+# print('Added mass coefficient Cmy =', CmNumy)
+# print('Added dumping coefficient Cvy =', CvNumy)
+
+DataOut1 = np.column_stack((CmNumy,CvNumy))
+np.savetxt('Numerical_coefficients.txt', DataOut1)
+
+
+# Save DataOut in .txt file
+
+Cm=10.16
+Cv=0.
+F = -rho*np.pi*Lmax*Ri**2.*u*w**2.*(-Cm*np.sin(w*t) + Cv*np.cos(w*t)) #Force vector
+DataOut = np.column_stack((t,F))
+np.savetxt('Theoretical_force.txt', DataOut) #this command overwrite the previus .txt file
 
 
