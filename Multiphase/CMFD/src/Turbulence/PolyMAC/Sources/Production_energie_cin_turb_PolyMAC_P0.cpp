f055e9ef69d045d99458c143b2285afebf7c7e16
triou <triou@cea.fr>
2022-11-30 10:25:23 +0100

    Update XDATA tags

--- Multiphase/CMFD/src/Turbulence/PolyMAC/Sources/Production_energie_cin_turb_PolyMAC_P0.cpp
+++ Multiphase/CMFD/src/Turbulence/PolyMAC/Sources/Production_energie_cin_turb_PolyMAC_P0.cpp
@@ -41,7 +41,7 @@
 #include <Taux_dissipation_turbulent.h>
 
 Implemente_instanciable(Production_energie_cin_turb_PolyMAC_P0,"Production_energie_cin_turb_Elem_PolyMAC_P0", Source_base);
-// XD Production_energie_cin_turb source_base Production_energie_cin_turb 0 Production source term for the TKE equation
+// XD Production_energie_cin_turb source_base Production_energie_cin_turb 1 Production source term for the TKE equation
 
 
 Sortie& Production_energie_cin_turb_PolyMAC_P0::printOn(Sortie& os) const
