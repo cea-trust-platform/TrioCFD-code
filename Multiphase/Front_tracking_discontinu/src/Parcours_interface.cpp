5a7371d0109567478dc7fa232b09ec5b8226563a
Fabrice BUFFA <fabrice.buffa@cea.fr>
2023-01-11 09:06:39 +0100

    Integration review

--- Multiphase/Front_tracking_discontinu/src/Parcours_interface.cpp
+++ Multiphase/Front_tracking_discontinu/src/Parcours_interface.cpp
@@ -1244,10 +1244,8 @@ int Parcours_interface::calcul_intersection_facelem_3D(
   }
   // En cas d'erreur d'arrondi ...
   if (surface < 0.) surface = 0.;
+  
   // calcul des contributions.
-  //  Modif BM 9/9/2004 : calcul d'une surface reelle et non d'une fraction!
-  // double normale[3];
-  //const double surface_facette = maillage.calcul_normale_3D(num_facette, normale);
   double barycentre_phase1[3] = {0.,0.,0.};
   if (correction_parcours_thomas_ || (sqrt(surface) > 5. * Erreur_max_coordonnees_))
     {
