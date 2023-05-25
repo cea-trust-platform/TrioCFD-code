d571b76178d75133958ed632d97578a5a985d6f3
mb124139 <michel.belliard@cea.fr>
2023-05-25 17:52:14 +0200

    u_star EF

--- src/Turbulence/Specializations/EF/Lois_Paroi/Hydr/Paroi_hyd_base_EF.cpp
+++ src/Turbulence/Specializations/EF/Lois_Paroi/Hydr/Paroi_hyd_base_EF.cpp
@@ -56,6 +56,73 @@ Entree& Paroi_hyd_base_EF::readOn(Entree& s)
   return s ;
 }
 
+/*##################################################################################################
+####################################################################################################
+################################# POSTRAITEMENT ####################################################
+####################################################################################################
+##################################################################################################*/
+
+void Paroi_hyd_base_EF::creer_champ(const Motcle& motlu)
+{
+  if (motlu=="u_star" && !champ_u_star_.non_nul())
+    {
+      int nb_comp = 1;
+      Noms noms(1);
+      noms[0]="u_star";
+      Noms unites(1);
+      unites[0] = "m/s";
+      double temps=0.;
+      const Equation_base& equation=mon_modele_turb_hyd->equation();
+      const Discretisation_base& discr = equation.probleme().discretisation();
+      discr.discretiser_champ("champ_elem",equation.domaine_dis(),scalaire,noms,unites,nb_comp,temps,champ_u_star_);
+      champs_compris_.ajoute_champ(champ_u_star_);
+    }
+}
+
+const Champ_base& Paroi_hyd_base_EF::get_champ(const Motcle& nom) const
+{
+  const Domaine_EF& domaine_EF = le_dom_EF.valeur();
+  const IntTab& face_voisins = domaine_EF.face_voisins();
+  if (nom==champ_u_star_.le_nom())
+    {
+      // Initialisation a 0 du champ volumique u_star
+      DoubleTab& valeurs = champ_u_star_->valeurs();
+      valeurs=0;
+      const Equation_base& my_eqn=mon_modele_turb_hyd->equation();
+      if (tab_u_star_.size_array()>0)
+        {
+          // Boucle sur les frontieres pour recuperer u_star si tab_u_star dimensionne
+          const Domaine_Cl_dis& zcl=my_eqn.domaine_Cl_dis();
+          int nb_front=my_eqn.domaine_dis().valeur().nb_front_Cl();
+          for (int n_bord=0; n_bord<nb_front; n_bord++)
+            {
+              const Cond_lim& la_cl = zcl.les_conditions_limites(n_bord);
+              if (sub_type(Dirichlet_paroi_fixe,la_cl.valeur()) )
+                {
+                  const Front_VF& le_bord = ref_cast(Front_VF,la_cl.frontiere_dis());
+                  int ndeb = le_bord.num_premiere_face();
+                  int nfin = ndeb + le_bord.nb_faces();
+                  for (int num_face=ndeb; num_face<nfin; num_face++)
+                    {
+                      int elem = face_voisins(num_face,0);
+                      valeurs(elem)=tab_u_star_(num_face);
+                    }
+                }
+            }
+        }
+      valeurs.echange_espace_virtuel();
+      // Met a jour le temps du champ:
+      champ_u_star_.mettre_a_jour(my_eqn.schema_temps().temps_courant());
+      return champs_compris_.get_champ(nom);
+    }
+  else
+    return champs_compris_.get_champ(nom);
+}
+
+void Paroi_hyd_base_EF::get_noms_champs_postraitables(Noms& nom,Option opt) const
+{
+  Turbulence_paroi_base::get_noms_champs_postraitables(nom,opt);
+}
 
 /////////////////////////////////////////////////////////////////////
 //
