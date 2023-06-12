6242285f522d82a364d908ac1a5453e5648cf4a2
Fabrice BUFFA <fabrice.buffa@cea.fr>
2023-06-12 14:38:41 +0200

    Update 'model reports'

--- share/doc_src/model_report/generer_model_report.sh
+++ share/doc_src/model_report/generer_model_report.sh
@@ -13,10 +13,10 @@ pdflatex ${DOC}.tex
 pdflatex ${DOC}.tex
 
 # Installation of the final pdf report
-cp ${DOC}.pdf ../../doc
+#cp ${DOC}.pdf ../../doc
 
 # Cleaning
-for ext in aux bbl blg idx log lot toc pdf
+for ext in aux bbl blg idx log lot toc
 do
   rm "${DOC}.${ext}"
 done
