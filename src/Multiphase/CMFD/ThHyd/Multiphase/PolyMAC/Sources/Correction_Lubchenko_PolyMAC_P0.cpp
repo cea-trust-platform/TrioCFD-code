357166938e371bf59e60cd1b20b6c2c92579f7f8
Corentin Reiss <corentin.reiss@cea.fr>
2023-10-04 17:54:09 +0200

    [EOS] sigma in interfacial forces

--- src/Multiphase/CMFD/ThHyd/Multiphase/PolyMAC/Sources/Correction_Lubchenko_PolyMAC_P0.cpp
+++ src/Multiphase/CMFD/ThHyd/Multiphase/PolyMAC/Sources/Correction_Lubchenko_PolyMAC_P0.cpp
@@ -143,12 +143,10 @@ void Correction_Lubchenko_PolyMAC_P0::ajouter_blocs_disp(matrices_t matrices, Do
           {
             Saturation_base& z_sat = milc.get_saturation(k, l);
             const int ind_trav = (k*(N-1)-(k-1)*(k)/2) + (l-k-1); // Et oui ! matrice triang sup !
-            // XXX XXX XXX
-            // Attention c'est dangereux ! on suppose pour le moment que le champ de pression a 1 comp. Par contre la taille de res est nb_max_sat*nbelem !!
-            // Aussi, on passe le Span le nbelem pour le champ de pression et pas nbelem_tot ....
-            assert(press.line_size() == 1);
-            assert(temp.line_size() == N);
-            z_sat.get_sigma(temp.get_span_tot(), press.get_span_tot(), Sigma_tab.get_span_tot(), nb_max_sat, ind_trav);
+            // recuperer sigma ...
+            const DoubleTab& sig = z_sat.get_sigma_tab();
+            // fill in the good case
+            for (int ii = 0; ii < ne_tot; ii++) Sigma_tab(ii, ind_trav) = sig(ii);
           }
         else if (milc.has_interface(k, l))
           {
