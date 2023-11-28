5dc85f71373b02ed86a225797a407f3b65a07829
Fabrice BUFFA <fabrice.buffa@cea.fr>
2023-11-28 01:08:08 +0100

    [Turbulence] Clean turbulence sources

--- src/TRUST_Modifs/Paroi_hyd_base_VEF.h
+++ src/TRUST_Modifs/Paroi_hyd_base_VEF.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2015 - 2016, CEA
+* Copyright (c) 2023, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -12,57 +12,39 @@
 * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 *****************************************************************************/
-//////////////////////////////////////////////////////////////////////////////
-//
-// File:        Paroi_hyd_base_VEF.h
-// Directory:   $TURBULENCE_ROOT/src/Specializations/VEF/Lois_Paroi/Hydr
-//
-//////////////////////////////////////////////////////////////////////////////
-
-
 
 #ifndef Paroi_hyd_base_VEF_included
 #define Paroi_hyd_base_VEF_included
 
 #include <Turbulence_paroi_base.h>
-#include <Domaine_VEF.h>
 #include <Domaine_Cl_VEF.h>
+#include <Domaine_VEF.h>
 #include <TRUST_Ref.h>
-
 
 /*! @brief CLASS: Paroi_hyd_base_VEF Classe de base des lois de paroi hydraulique en VEF
  *
- * .SECTION  voir aussi
- *  Turbulence_paroi_base
- *
  */
-class Paroi_hyd_base_VEF : public Turbulence_paroi_base
+class Paroi_hyd_base_VEF: public Turbulence_paroi_base
 {
-
   Declare_base(Paroi_hyd_base_VEF);
-
 public:
 
-  void associer(const Domaine_dis& ,const Domaine_Cl_dis& ) override;
+  void associer(const Domaine_dis&, const Domaine_Cl_dis&) override;
   void init_lois_paroi_();
   DoubleTab& corriger_derivee_impl(DoubleTab& d) const override;
-  inline const ArrOfInt& face_keps_imposee() const
-  {
-    return face_keps_imposee_ ;
-  };
-  void imprimer_premiere_ligne_ustar(int boundaries_, const LIST(Nom)& boundaries_list, const Nom& nom_fichier_) const override;
-  void imprimer_ustar_mean_only(Sortie&, int, const LIST(Nom)&, const Nom& ) const override;
+  inline const ArrOfInt& face_keps_imposee() const { return face_keps_imposee_; }
+  void imprimer_premiere_ligne_ustar(int boundaries_, const LIST(Nom) &boundaries_list, const Nom& nom_fichier_) const override;
+  void imprimer_ustar_mean_only(Sortie&, int, const LIST(Nom)&, const Nom&) const override;
 
 protected:
-
   REF(Domaine_VEF) le_dom_VEF;
   REF(Domaine_Cl_VEF) le_dom_Cl_VEF;
   IntVect face_keps_imposee_; // avec descripteur parallele
-  int flag_face_keps_imposee_;
+  int flag_face_keps_imposee_ = 0;
 
   static constexpr double BETA_OMEGA {0.075};
   static constexpr double BETA_K {0.09};  // equals to Cmu
 
 };
 
-#endif
+#endif /* Paroi_hyd_base_VEF_included */
