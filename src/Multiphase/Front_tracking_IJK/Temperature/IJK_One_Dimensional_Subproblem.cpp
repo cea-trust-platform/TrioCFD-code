559626f22420f73413ecb77d7d75dbea041a3f9a
triou <trust@cea.fr>
2024-03-12 12:53:48 +0100

    [IJKFT] Fix initialization error in IJK_One_Dimensional_Subproblem::correct_tangential_temperature_hessian

--- src/Multiphase/Front_tracking_IJK/Temperature/IJK_One_Dimensional_Subproblem.cpp
+++ src/Multiphase/Front_tracking_IJK/Temperature/IJK_One_Dimensional_Subproblem.cpp
@@ -3020,7 +3020,7 @@ void IJK_One_Dimensional_Subproblem::correct_tangential_temperature_gradient(Dou
 
 void IJK_One_Dimensional_Subproblem::correct_tangential_temperature_hessian(DoubleVect& tangential_diffusion_source_terms)
 {
-  DoubleVect tangential_diffusion_source_terms_tmp = tangential_diffusion_source_terms_tmp;
+  DoubleVect tangential_diffusion_source_terms_tmp = tangential_diffusion_source_terms;
   for (int i=0; i<tangential_diffusion_source_terms_tmp.size(); i++)
     tangential_diffusion_source_terms_tmp[i] -= tangential_diffusion_source_terms_tmp[0];
 }
