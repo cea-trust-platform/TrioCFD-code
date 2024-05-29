2f25ce9c1d3d710c6c61bda8233e8b67f6ea434b
triou <trust@cea.fr>
2024-05-29 11:34:27 +0200

    [Validation] fix test_convection_masse with visit 3.3.3

--- share/Validation/Rapports_automatiques/Algo_QC/test_convection_masse/src/test_convection_masse/instructions.py
+++
@@ -1,6 +0,0 @@
-# Instructions visit
-DefineScalarExpression("x","coord(grid_geom2)[0]")
-DefineScalarExpression("y","coord(grid_geom2)[1]")
-DefineScalarExpression("z","coord(grid_geom2)[2]")
-DefineScalarExpression("simu_d_rho","DRHO_DT_ELEM_grid_geom2")
-
