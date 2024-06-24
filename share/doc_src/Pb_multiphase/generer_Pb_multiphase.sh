839727618fcc1550ebce929337670720db70f1ee
Julie Darona <julie.darona@cea.fr>
2024-06-24 19:08:12 +0200

    [DELIVERY] Update documentation for v1.9.4 delivery
    TrioCFD v1.9.3 merge 67
    
    Based on trust Id SHA1 : see ./src/Version_info

---
+++ share/doc_src/Pb_multiphase/generer_Pb_multiphase.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Generation documentation des modeles format note CEA
+#DOC="TrioCFD_Modeles_clsDM2S"
+
+# Generation documentation des modeles format document utilisateur
+DOC="TrioCFD_Pb_multiphase"
+
+# Generation of the pdf report
+pdflatex ${DOC}.tex
+bibtex ${DOC}
+pdflatex ${DOC}.tex
+pdflatex ${DOC}.tex
+
+# Installation of the final pdf report
+#cp ${DOC}.pdf ../../doc
+
+# Cleaning
+for ext in aux bbl blg idx log lot toc
+do
+  rm "${DOC}.${ext}"
+done
+
+exit 0
